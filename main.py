3ff693e196c0b2f826d42c58fcc7ac012eb028ef
lorenzo-mora <l.mora4@campus.unimib.it>
2024-06-20 20:00:21 +0200

    Added options in main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from train.training_model import main
+import argparse
+from pathlib import Path
+from train.training_model import main, CONFIG_PATH
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the training model.")
+    parser.add_argument('-t', '--training_config_file', type=Path,
+                        default=CONFIG_PATH.joinpath("train_config.json"),
+                        help="Path to the training configuration file.")
+    parser.add_argument('-d', '--data_config_file', type=Path, 
+                        default=CONFIG_PATH.joinpath("data_config.json"),
+                        help="Path to the data configuration file.")
 
-    train_manager_obj = main()
+    args = parser.parse_args()
+
+    train_manager_obj = main(training_config_file=args.training_config_file,
+                             data_config_file=args.data_config_file)
